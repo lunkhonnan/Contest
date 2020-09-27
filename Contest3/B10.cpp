5ac0dfc3418ea33092651fb605e242a2e4a7ff4b
lunkhonnan <outplay2000@gmail.com>
2020-09-27 17:16:39 +0700

    add B10

--- Contest3/B10.cpp
+++ Contest3/B10.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-void nhapMang(int a[],int n) {
-    for(int i=0;i<n;i++)
-        cin>>a[i];
+
+#define MAX 100000
+
+int main() {
+    int a[MAX];
+    int n;
+    int t;
+    cin>>t;
+    while(t-->0) {
+        cin>>n;
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        long long sum = 0;
+        while(n != 1) {
+            int i = n-1;
+            sort(a,a+n,greater<int>());
+            a[i-1] = a[i] + a[i-1];
+            sum += a[i-1];
+            n--;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
 }
